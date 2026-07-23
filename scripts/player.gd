c1f4fa1e23e65cbca22fc62b8c41f1d3cf3e2f62
PAT47 <pgonzalves08@gmail.com>
2026-07-23 16:08:06 +0530

    scripts/player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,12 +7,11 @@ const FRICTION = 1200.0
 func _physics_process(delta): 
 	
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	print(direction)
+	
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	move_and_slide()
-	print(global_position)
-	print(velocity)
+	
